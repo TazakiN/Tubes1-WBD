5dfedcc37d927f628d435ed4275ef0622c0e5101
TazakiN <tazkia.nizami@gmail.com>
2024-10-14 09:48:57 +0700

    feat: db setup

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Install php dulu, kalo udah install php, ikutin langkah berikut :
 3. Masuk ke folder src
 4. Jalanin perintah 'php -S localhost:8000'
 5. Buka browser dan ketik 'localhost:8000'
+
+## Setup Database
+
+pastiin udah punya PostgreSQL
+
+1. Masuk ke folder database
+2. Jalanin perintah 'psql -U postgres -f init.sql'
+3. udah sih, tinggal cek
---
+++ php/src/database/init.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE LinkInPurry;
+USE LinkInPurry;
+
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) CHECK (role IN ('company', 'jobseeker')) NOT NULL,
+    nama VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Company_Detail (
+    user_id INT PRIMARY KEY,
+    lokasi VARCHAR(255) NOT NULL,
+    about VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Lowongan (
+    lowongan_id SERIAL PRIMARY KEY,
+    company_id INT NOT NULL,
+    posisi VARCHAR(255) NOT NULL,
+    deskripsi TEXT NOT NULL,
+    jenis_pekerjaan VARCHAR(255) NOT NULL,
+    jenis_lokasi VARCHAR(255) CHECK (jenis_lokasi IN ('on-site', 'remote')) NOT NULL,
+    is_open BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_timestamp
+BEFORE UPDATE ON Lowongan
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TABLE Attachment_Lowongan (
+    attachment_id SERIAL PRIMARY KEY,
+    lowongan_id INT NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (lowongan_id) REFERENCES Lowongan(lowongan_id)
+);
+
+CREATE TABLE Lamaran (
+    lamaran_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    lowongan_id INT NOT NULL,
+    cv_path VARCHAR(255) NOT NULL,
+    video_path VARCHAR(255) NOT NULL,
+    status VARCHAR(255) CHECK (status IN ('accepted', 'rejected', 'waiting')),
+    status_reason TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
