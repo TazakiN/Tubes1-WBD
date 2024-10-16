060927109efc5811fddfbd0c96ecf7bd8d395ad6
TazakiN <tazkia.nizami@gmail.com>
2024-10-16 15:50:45 +0700

    fix: update foreign keys in database

--- php/src/database/init.sql
+++ php/src/database/init.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE LinkInPurry;
-USE LinkInPurry;
+\c LinkInPurry;
 
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
     role VARCHAR(255) CHECK (role IN ('company', 'jobseeker')) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     nama VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE Company_Detail (
     user_id INT PRIMARY KEY,
     lokasi VARCHAR(255) NOT NULL,
-    about VARCHAR(255) NOT NULL,
+    about TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
@@ -22,10 +22,11 @@ CREATE TABLE Lowongan (
     posisi VARCHAR(255) NOT NULL,
     deskripsi TEXT NOT NULL,
     jenis_pekerjaan VARCHAR(255) NOT NULL,
-    jenis_lokasi VARCHAR(255) CHECK (jenis_lokasi IN ('on-site', 'remote')) NOT NULL,
+    jenis_lokasi VARCHAR(255) CHECK (jenis_lokasi IN ('on-site', 'remote', 'hybrid')) NOT NULL,
     is_open BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (company_id) REFERENCES Users(user_id)
 );
 
 CREATE OR REPLACE FUNCTION update_timestamp()
@@ -56,5 +57,7 @@ CREATE TABLE Lamaran (
     video_path VARCHAR(255) NOT NULL,
     status VARCHAR(255) CHECK (status IN ('accepted', 'rejected', 'waiting')),
     status_reason TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (lowongan_id) REFERENCES Lowongan(lowongan_id)
 );
