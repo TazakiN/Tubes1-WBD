01b1e47c8c79cbb5c5addf2d5a0b95ee2af7810b
TazakiN <tazkia.nizami@gmail.com>
2024-10-16 15:51:34 +0700

    feat: implement RegisterController

---
+++ php/src/app/controllers/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\controllers;
+
+use app\controllers\BaseController;
+use app\repositories\CompanyDetailRepository;
+use app\services\UserService;
+use Exception;
+
+class RegisterController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct(UserService::getInstance(), CompanyDetailRepository::getInstance());
+    }
+
+    public function get($urlParams)
+    {
+        if (isset($_SESSION["user_id"])) {
+            parent::redirect("/");
+        } else {
+            parent::render($urlParams, "register", "layouts/base");
+        }
+    }
+
+    protected function post($urlParams)
+    {
+        try {
+            if ($_POST['role'] == "jobseeker") {
+                $this->service->registerJobSeeker(
+                    $_POST['role'],
+                    $_POST['nama'],
+                    $_POST['email'],
+                    $_POST['password'],
+                    $_POST['confirm_password']
+                );
+            } else {
+                $this->service->registerCompany(
+                    $_POST['role'],
+                    $_POST['nama'],
+                    $_POST['email'],
+                    $_POST['password'],
+                    $_POST['confirm_password'],
+                    $_POST['lokasi'],
+                    $_POST['about']
+                );
+            }
+            parent::redirect("/login");
+        } catch (Exception $e) {
+            $msg = $e->getMessage();
+            echo "<script>console.error('Error: " . addslashes($msg) . "');</script>";
+            parent::redirect("/register");
+            parent::render($urlParams, "register", "layouts/base", array(
+                'msg' => $msg
+            ));
+        }
+    }
+};
