c5ba5399c474a7e3c9051bc231be599e59f61aec
TazakiN <tazkia.nizami@gmail.com>
2024-10-22 17:34:15 +0700

    feat: implement lowongan page fe

--- php/src/app/views/lowongan-detail-company.php
+++ php/src/app/views/lowongan-detail-company.php
@@ -1,11 +1,33 @@
 <?php
-    $__headContent = ''
+    $__headContent = '<link rel="stylesheet" href="/public/css/lowongan-detail.css">';
 ?>
 
 <section class="lowongan-detail-section">
-    <div class="company-info">
-        <img src="/public/svg/company.svg" alt="Company Logo">
-        halohalohalo
-    </div>
+    <div class="container">
+        <?php
+            include "lowongan-detail.php";
+        ?>
 
+        <button class="action-button">Edit</button>
+        
+        <!-- TODO: Masih Placeholder -->
+        <div class="applicants">
+            <h2>Applicants</h2>
+            <div class="applicant">
+                <span class="applicant-name">Tazkia Ganteng Banget</span>
+                <div class="applicant-status">
+                    <span class="status waiting">Waiting</span>
+                    <button class="details-button">Details</button>
+                </div>
+            </div>
+            <div class="applicant">
+                <span class="applicant-name">Farhan Seksi Banget</span>
+                <div class="applicant-status">
+                    <span class="status waiting">Waiting</span>
+                    <button class="details-button">Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+    
---
+++ php/src/app/views/lowongan-detail.php
@@ -0,0 +1,48 @@
+        <div class="header">
+            <img src="/public/svg/company.svg" alt="Company Logo">
+            <h1><?php echo $data["nama"] ?></h1>
+        </div>
+        <?php
+            if ($data["is_open"]) {
+                echo '<div class="status open">Open</div>';
+            } else {
+                echo '<div class="status closed">Closed</div>';
+            }
+        ?>
+        
+        <h1 class="job-title"><?php echo $data["posisi"] ?></h1>
+        <div class="job-info">
+            <span class="job-type">
+                <img src="/public/svg/location.svg" alt="Location SVG">    
+                <?php echo $data["jenis_lokasi"] ?>
+            </span>
+            <span class="job-category">
+                <img src="/public/svg/job-case.svg" alt="Job Category SVG">    
+                <?php echo $data["jenis_pekerjaan"] ?>
+            </span>
+            <span class="job-date">
+                <img src="/public/svg/calendar.svg" alt="Job Category SVG">
+                Posted: <?php echo $data["created_at"]; ?> | 
+                Updated: <?php echo $data["updated_at"]; ?>
+            </span>
+        </div>
+        
+        <div class="description">
+            <?php echo $data["deskripsi"] ?>
+        </div>
+        
+        <div class="attachments">
+            <h2>Attachments</h2>
+            <div class="images">
+                <?php foreach ($data['attachments'] as $attachment): ?>
+                    <div class="image-container">
+                        <!-- Menampilkan gambar -->
+                        <img src="/uploads/<?php echo htmlspecialchars($attachment->file_path); ?>" 
+                            alt="Attachment Image" class="attachment-image" />
+
+                        <!-- Menampilkan nama file -->
+                        <p class="file-name"><?php echo htmlspecialchars(string: explode("_", $attachment->get('file_path'))[1]); ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
