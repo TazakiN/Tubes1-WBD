c3a181dadf4740522e0864c2813e960b8b73de70
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2024-10-25 18:54:01 +0700

    fix : responsive home jobseeker

--- php/src/app/views/home-lowongan-jobseeker.php
+++ php/src/app/views/home-lowongan-jobseeker.php
@@ -30,7 +30,7 @@
         <?php foreach($data["lowongans"] as $k => $v): ?>
             <div class="job-card" onclick="window.location.href='/lowongan?lowongan_id=<?= $v->lowongan_id ?>'">
                 <div class="profile-icon">
-                    <svg width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                    <svg class="svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                         <circle cx="12" cy="7" r="4"></circle>
                     </svg>
